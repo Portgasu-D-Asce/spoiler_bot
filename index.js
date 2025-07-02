6d6e149b9370cc5a8d3b9a3f78a6324636e880df
ム s̷  ᑢ Ｅ <107835078+Portgasu-D-Asce@users.noreply.github.com>
2025-07-02 10:28:28 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} is online`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith('!a ')) {
+    const text = message.content.slice(3).trim(); 
+    if (!text) return message.reply("❌ Please provide a message.");
+
+    
+    const webhook = await message.channel.createWebhook({
+      name: 'Anonymous',
+      avatar: '', 
+    });
+
+    await webhook.send({
+      content: text,
+    });
+
+    await webhook.delete();  
+
+    await message.delete(); 
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
